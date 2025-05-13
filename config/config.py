14eb1beee4da64dd78e9616e2e2c62f12768b87c
panjc <panjc_job@163.com>
2025-05-13 12:55:36 +0800

    Update database configuration to use 'db_tool.db' instead of 'flask_shop.db'

--- config/config.py
+++ config/config.py
@@ -7,7 +7,7 @@ class Config:
     # 数据库配置
     BASE_DIR = os.path.abspath(os.path.dirname(os.path.dirname(__file__)))
     SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or \
-        'sqlite:///' + os.path.join(BASE_DIR, 'flask_shop.db')
+        'sqlite:///' + os.path.join(BASE_DIR, 'db_tool.db')
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     
     # JWT配置
